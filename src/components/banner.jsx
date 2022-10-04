91b18cbdd843d0e9471605459053af1084c51900
fz <phiane.d@gmail.com>
2022-10-04 18:09:58 +0200

    text align banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -4,7 +4,8 @@ import banner from "../data/banner.png";
 const Banner = () => {
   return (
     <div className="banner">
-      <p className="banner_text">Chez vous, partout et ailleurs</p>
+      <img className="banner_img" src={banner} alt='paysage'></img>
+      <div className="banner_text">Chez vous, partout et ailleurs</div>
     </div>
   );
 };
