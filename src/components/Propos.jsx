e886adcf081ec46a50c60c4a7ace28620f2daf75
fz <phiane.d@gmail.com>
2022-10-12 16:08:11 +0200

    reglage partie a propos responsive

--- src/components/Propos.jsx
+++ src/components/Propos.jsx
@@ -1,11 +1,14 @@
 import Collapsible from "./collapsible";
-import propos from'../data/assets/propos.jpg'
-import "../styles/layout/propos.scss"
+import propos from "../data/assets/propos.jpg";
+import "../styles/layout/propos.scss";
 
 const Propos = () => {
   return (
-    <div className="body">
-      <div className="propos"><img className="propos_img" src={propos} alt='paysage montagne'></img></div>
+    <div className="body_propos">
+      <div className="propos">
+        <img className="propos_img" src={propos} alt="paysage montagne"></img>
+      </div>
+    <div className="body_propos_collasible">
       <Collapsible label="Fiabilité">
         <p className="text">
           Les annonces postées sur Kasa garantissent une fiabilité totale. Les
@@ -14,21 +17,21 @@ const Propos = () => {
         </p>
       </Collapsible>
       <Collapsible label="Respect">
-      <p className="text">
+        <p className="text">
           La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
           comportement discriminatoire ou de perturbation du voisinage
           entraînera une exclusion de notre plateforme.
         </p>
       </Collapsible>
       <Collapsible label="Service">
-      <p className="text">
+        <p className="text">
           Nos équipes se tiennent à votre disposition pour vous fournir une
           expérience parfaite. N'hésitez pas à nous contacter si vous avez la
           moindre question.
         </p>
       </Collapsible>
       <Collapsible label="Sécurité">
-      <p className="text">
+        <p className="text">
           La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
           pour les voyageurs, chaque logement correspond aux critères de
           sécurité établis par nos services. En laissant une note aussi bien à
@@ -37,6 +40,7 @@ const Propos = () => {
           sur la sécurité domestique pour nos hôtes.
         </p>
       </Collapsible>
+    </div>
     </div>
   );
 };
--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,11 +1,13 @@
-import "../styles/layout/banner.scss"
+import "../styles/layout/banner.scss";
 import banner from "../data/assets/banner.png";
 
 const Banner = () => {
   return (
     <div className="banner">
-      <img className="banner_img" src={banner} alt='paysage'></img>
-      <div className="banner_text">Chez vous, partout et ailleurs</div>
+      <img className="banner_img" src={banner} alt="paysage"></img>
+      <div className="banner_text">
+        Chez vous, <span className="text_break">partout et ailleurs</span>
+      </div>
     </div>
   );
 };
