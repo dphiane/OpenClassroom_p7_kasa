3b513319dfeae6be77a60ce1cf3b4f09549dd659
fz <phiane.d@gmail.com>
2022-10-12 11:27:52 +0200

    slides numbers

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,44 +1,44 @@
-import { useState } from 'react'
-import {ReactComponent as Prev} from "../data/icon/Vectorleft.svg"
-import {ReactComponent as Next} from '../data/icon/Vectorright.svg'
-import "../styles/components/carousel.scss"
+import { useState } from "react";
+import { ReactComponent as Prev } from "../data/icon/Vectorleft.svg";
+import { ReactComponent as Next } from "../data/icon/Vectorright.svg";
+import "../styles/components/carousel.scss";
 
-const Slider=({slides})=>{
-    const [current,setCurrent]=useState(0)
-    const length=slides.length
-    const nextSlide=()=>{
-        setCurrent(current=== length -1 ? 0: current + 1)
-    }
-    const prevSlide=()=>{
-        setCurrent(current === 0 ? length - 1: current - 1)
-    }
-    function showHide(props){
-      if (length>1){
-        return props
-      }else{
-        return null
-      }
-    }
-    return(
-        <div className='img_container'>
-        {showHide(<Prev className="prev" onClick={prevSlide}/>)}
-        {showHide(<Next className="next" onClick={nextSlide}/>)}
-        {slides.map((slide, index) => {
-          return (
-            <div className='carousel_inner' key={index}>
-              {index === current && (
-                <img className='img_slide'src={slide} alt="logement"></img>
-              )
-              }
-              <div className='index'>{current+1}/{index+1}</div>
-              
-            </div>
-          );
-        })}
-        
-        </div>
-        
-    );
+const Slider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
   };
-  
-  export default Slider;
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  function showHide(props) {
+    if (length > 1) {
+      return props;
+    } else {
+      return null;
+    }
+  }
+  return (
+    <div className="img_container">
+      {showHide(<Prev className="prev" onClick={prevSlide} />)}
+      {showHide(<Next className="next" onClick={nextSlide} />)}
+      {slides.map((slide, index) => {
+        return (
+          <div className="carousel_container" key={index}>
+            {index === current && (
+              <div className="carousel_inner">
+                <img className="img_slide" src={slide} alt="logement"></img>
+                <p className="index">
+                  {current + 1}/{slides.length}
+                </p>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Slider;
